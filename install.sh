348b52b8aeaaafdbe7ac6b1bf589ede388e960e6
tdcomcl <40880214+tdcomcl@users.noreply.github.com>
2024-08-14 13:27:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,24 +9,37 @@ set -e
 # Install Packages
 install_packages() {
     # if we received packages to install, install them
-    [ $# -eq 0 ] && echo "* No packages requested to install" || {
+    if [ $# -eq 0 ]; then 
+        echo "* No packages requested to install"
+    else
         # Local variable to hold our packages
         local packages
         # Loop through our passed packages
         while [ $# -gt 0 ]; do
             # Check if the command exists, if not flag it for installation
-            [ -x "$(command -v "${1}")" ] && echo "* ${1} is already installed" || { echo "* ${1} will be installed"; packages="${packages} ${1}"; }
+            if [ -x "$(command -v "${1}")" ]; then
+                echo "* ${1} is already installed"
+            else
+                echo "* ${1} will be installed"
+                packages="${packages} ${1}"
+            fi
             # Shift to the next package
             shift
         done
         # Check if we have any packages to install
-        [ -z "${packages}" ] && { echo "* All requested packages are already installed"; return 0; } || { echo "* Installing ${packages:1}"; apt update; apt install -y ${packages}; }
-    }
+        if [ -z "${packages}" ]; then
+            echo "* All requested packages are already installed"
+            return 0
+        else
+            echo "* Installing ${packages:1}"
+            apt update
+            apt install -y ${packages}
+        fi
+    fi
 }
 
 # Functions
-frpc_system()
-{
+frpc_system() {
     # Link to our hosted frpc.service (since it was removed from the release)
     FRPC_SERVICE_URL="https://raw.githubusercontent.com/tdcomcl/frpc/main/frpc.service"
 
@@ -42,7 +55,6 @@ frpc_system()
     echo "* Filename: ${FRPC_FILENAME}"
     echo "* Install Directory: ${FRPC_DIRECTORY}"
     echo "* Download URL: ${FRPC_URL}"
- 
 
     # Set /tmp as active directory and do our tasks
     pushd /tmp
@@ -53,29 +65,33 @@ frpc_system()
     frpc_config
 
     # If frpc is not already installed, install it
-    [ -f /usr/bin/frpc ] || cp "${FRPC_DIRECTORY}"/frpc /usr/bin/frpc
+    if [ ! -f /usr/bin/frpc ]; then
+        cp "${FRPC_DIRECTORY}"/frpc /usr/bin/frpc
+        chmod +x /usr/bin/frpc
+    fi
 
     # If the frpc service is not active, install it and start it, otherwise restart it
-    [ "$(systemctl show -p ActiveState frpc | sed 's/ActiveState=//g')" != "active" ] && {
+    if [ "$(systemctl show -p ActiveState frpc | sed 's/ActiveState=//g')" != "active" ]; then
         echo "* Installing, enabling and starting frpc service"
         curl -L "${FRPC_SERVICE_URL}" -o /etc/systemd/system/frpc.service
-        systemctl daemon-reload;
-        systemctl enable frpc;
-        systemctl start frpc;
-    } || {
+        systemctl daemon-reload
+        systemctl enable frpc
+        systemctl start frpc
+    else
         echo "* Wrote new config; restarting frpc"
-        systemctl restart frpc;
-    }
+        systemctl restart frpc
+    fi
 
     # Cleanup and pop out of /tmp
     rm -rf "${FRPC_DIRECTORY}" "${FRPC_FILENAME}"
     popd
 }
 
-frpc_docker()
-{
+frpc_docker() {
     # Check if docker is installed, if not install it
-    [ -x "$(command -v docker)" ] && echo "* Docker is already installed" || {
+    if [ -x "$(command -v docker)" ]; then
+        echo "* Docker is already installed"
+    else
         echo "* Installing Docker"
 
         # Install our required packages
@@ -83,7 +99,7 @@ frpc_docker()
 
         # Pull docker install script and run it
         curl -fsSL https://get.docker.com | sh
-    }
+    fi
     
     # Start our docker container for FRPC
     docker run --restart=always --network host -d \
@@ -94,38 +110,42 @@ frpc_docker()
       --name frpc privaterouterllc/frpc
 }
 
-frpc_config()
-{
-[ -d /etc/frp ] || mkdir -p /etc/frp
-cat > /etc/frp/frpc.ini <<-EOF
-[common]
-server_addr = ${FRP_SERVER}
-server_port = 12000
-token = ${FRP_TOKEN}
+frpc_config() {
+    if [ ! -d /etc/frp ]; then
+        mkdir -p /etc/frp
+    fi
 
-[http]
-type = tcp
-local_ip = 127.0.0.1
-local_port = 80
-remote_port = 80
+    cat > /etc/frp/frpc.ini <<-EOF
+    [common]
+    server_addr = ${FRP_SERVER}
+    server_port = 12000
+    token = ${FRP_TOKEN}
 
-[https]
-type = tcp
-local_ip = 127.0.0.1
-local_port = 443
-remote_port = 443
+    [http]
+    type = tcp
+    local_ip = 127.0.0.1
+    local_port = 80
+    remote_port = 80
+
+    [https]
+    type = tcp
+    local_ip = 127.0.0.1
+    local_port = 443
+    remote_port = 443
 EOF
-
+}
 
 # Check if we are root
-[ "$EUID" -ne 0 ] && { echo "Please run as root"; exit 1; }
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
 
-#  Read our script name
+# Read our script name
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 # Helper Sub
-show_help()
-{    
+show_help() {    
     echo "== ${SCRIPT_NAME} Flags (* Indicates Required) =="
     echo "* [-s 123.456.789.012]* sets the FRP Server Address"
     echo "* [-p 7000] sets the FRP Server Port"
@@ -137,7 +157,10 @@ show_help()
 }
 
 # Check if we have passed any arguments
-[ $# -eq 0 ] && show_help && exit 1
+if [ $# -eq 0 ]; then
+    show_help
+    exit 1
+fi
 
 # Iterate over arguments and process them
 while (( "${#}" )); do
@@ -147,19 +170,39 @@ while (( "${#}" )); do
             exit 0
             ;;
         -s|--server)
-            [[ ${2} != -* && ! -z ${2} ]] && { FRP_SERVER="${2}"; echo "Using FRP Server: ${2}"; } || { ERRORS+=("Invalid FRP Server passed to -s"); }
+            if [[ ${2} != -* && ! -z ${2} ]]; then
+                FRP_SERVER="${2}"
+                echo "Using FRP Server: ${2}"
+            else
+                ERRORS+=("Invalid FRP Server passed to -s")
+            fi
             shift
             ;;
         -p|--port)
-            [[ ${2} != -* && ! -z ${2} ]] && { FRP_PORT="${2}"; echo "Using FRP Port: ${2}"; } || { ERRORS+=("Invalid FRP Port passed to -p"); }
+            if [[ ${2} != -* && ! -z ${2} ]]; then
+                FRP_PORT="${2}"
+                echo "Using FRP Port: ${2}"
+            else
+                ERRORS+=("Invalid FRP Port passed to -p")
+            fi
             shift
             ;;
         -t|--token)
-            [[ ${2} != -* && ! -z ${2} ]] && { FRP_TOKEN="${2}"; echo "Using FRP Token: ${2}"; } || { ERRORS+=("Invalid FRP Token passed to -t"); }
+            if [[ ${2} != -* && ! -z ${2} ]]; then
+                FRP_TOKEN="${2}"
+                echo "Using FRP Token: ${2}"
+            else
+                ERRORS+=("Invalid FRP Token passed to -t")
+            fi
             shift
             ;;
         -v|--version)
-            [[ ${2} != -* && ! -z ${2} ]] && { FRPC_VERSION="${2}"; echo "Using FRP Version: ${2}"; } || { ERRORS+=("Invalid FRP Version passed to -v"); }
+            if [[ ${2} != -* && ! -z ${2} ]]; then
+                FRPC_VERSION="${2}"
+                echo "Using FRP Version: ${2}"
+            else
+                ERRORS+=("Invalid FRP Version passed to -v")
+            fi
             shift
             ;;
         -f|--force)
@@ -182,18 +225,26 @@ while (( "${#}" )); do
 done
 
 # If we are running docker install, skip this
-[ -z "${DOCKER}" ] && { 
+if [ -z "${DOCKER}" ]; then 
     # If force is not set and FRPC is already installed, exit
-    [[ -z "${FORCE}" && "$(systemctl show -p ActiveState frpc | sed 's/ActiveState=//g')" == "active" ]] && { 
+    if [[ -z "${FORCE}" && "$(systemctl show -p ActiveState frpc | sed 's/ActiveState=//g')" == "active" ]]; then 
         echo "* FRPC is already installed (use -f to override this)"
         exit 1
-    }
-}
+    fi
+fi
 
 # Check if our required arguments are set
-[ -z "${FRP_SERVER}" ] && { ERRORS+=("FRP Server is required"); }
-[ -z "${FRP_PORT}" ] && { FRP_PORT=7000; }
-[ -z "${FRP_TOKEN}" ] && { ERRORS+=("FRP Token is required"); }
+if [ -z "${FRP_SERVER}" ]; then 
+    ERRORS+=("FRP Server is required")
+fi
+
+if [ -z "${FRP_PORT}" ]; then 
+    FRP_PORT=7000
+fi
+
+if [ -z "${FRP_TOKEN}" ]; then 
+    ERRORS+=("FRP Token is required")
+fi
 
 # Print out our errors if we have any
 if [ ! -z "${ERRORS}" ]; then
@@ -201,27 +252,4 @@ if [ ! -z "${ERRORS}" ]; then
     for error in "${ERRORS[@]}"; do
         printf "\n= ${error}\n"
     done
-    exit 1
-fi
-
-# Determine our OS Architecture
-case "$(uname -m)" in
-    x86_64) FRPC_ARCH='amd64';;
-    aarch64) FRPC_ARCH='arm64';;
-    armv7l) FRPC_ARCH='arm';;
-    i686) [ -z "${DOCKER}" ] && FRPC_ARCH='386' || { echo "== !! Docker will not install on i686 systems"; exit 1; };;
-    *) echo "== !! unsupported architecture $(uname -m)"; exit 1 ;;
-esac
-
-# Show banner
-banner
-
-# If docker is flagged install docker frpc, otherwise install system version of frpc
-[ -z "${DOCKER}" ] && frpc_system || frpc_docker
-
-[ -z "${CLEAN}" ] || {
-    echo "Cleaning History"
-    history -w
-    cat /dev/null > ~/.bash_history
-    history -c
-}
+    exit 
